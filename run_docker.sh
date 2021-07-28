e6c0affaffca7633a1047786a21a6b732e216b36
Arnold <arnoldpayet@gmail.com>
2021-07-28 07:54:36 -0400

    TASK4: Upload the Docker Image

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=mlapi-1.0 .
+docker build --tag=mlmsapi .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 mlapi-1.0
+docker run -p 8000:80 mlmsapi
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="arnoldpaye/mlmsapi"
+tagversion="v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username arnoldpaye
+docker image tag mlmsapi $dockerpath:$tagversion
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath:$tagversion
