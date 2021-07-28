3392aa94968f13e023a6eb7566b12d644eabee97
Arnold <arnoldpayet@gmail.com>
2021-07-28 09:23:52 -0400

    TASK6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="arnoldpaye/mlmsapi"
+tagversion="v1.0.0"
+podname="mlmsapipod"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $podname --restart=Never --image $dockerpath:$tagversion --port 80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $podname 8000:80
